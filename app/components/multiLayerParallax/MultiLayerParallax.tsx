a154a93867d1130cd29eff4a8bc3c46fa6596544
jjjadey <nattakan.premp@gmail.com>
2024-07-22 14:19:00 +0700

    Layered parallax

--- app/components/multiLayerParallax/MultiLayerParallax.tsx
+++ app/components/multiLayerParallax/MultiLayerParallax.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 import styles from "./MultiLayerParallax.styles";
 
 function MultiLayerParallax() {
-  return (
-    <div className="w-full h-screen overflow-hidden relative grid place-items-center">
-      <h1 className="font-bold text-white text-7xl md:text-9xl relative z-10">
-        PARALLAX
-      </h1>
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
 
-      <div className="absolute inset-0 z-0" style={styles.imgFull} />
-      <div className="absolute inset-0 z-20" style={styles.imgBottom} />
+  //0: beginning of element, 1: end of element
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+
+  console.log("scrollYProgress", scrollYProgress);
+  console.log("backgroundY", backgroundY);
+  console.log("textY", textY);
+
+  return (
+    <div
+      ref={ref}
+      className="w-full h-screen overflow-hidden relative grid place-items-center"
+    >
+      <motion.h1
+        style={{ y: textY }}
+        className="font-bold text-white text-7xl md:text-9xl relative z-10"
+      >
+        PARALLAX
+      </motion.h1>
+
+      <motion.div
+        className="absolute inset-0 z-0"
+        style={{ ...styles.imgFull, ...{ y: backgroundY } }}
+      />
+      <motion.div className="absolute inset-0 z-20" style={styles.imgBottom} />
     </div>
   );
 }
